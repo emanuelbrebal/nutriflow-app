505ecbc70713f6fe7b8e5648c21cc40c64ae57fe
Emanuel <manu.brebal@gmail.com>
2025-08-31 21:30:22 -0300

    fix: Configuring the environment

--- config/database.php
+++ config/database.php
@@ -89,7 +89,7 @@ return [
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => str_replace('%23', '#', env('DB_PASSWORD', '')),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
