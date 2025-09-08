85bd83a8fb35397e8632610b34591c87ac6f864b
Emanuel <manu.brebal@gmail.com>
2025-09-08 13:02:41 -0300

    feat: First Views: Landing Page, Login, Register, User/MyDashBoard and Nutritionist/MyPatients
    New ts for formatting CPF and Mobile number at frontend

---
+++ resources/js/pages/Layouts/AuthLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="min-h-screen w-screen flex items-center justify-center bg-gray-100">
+    <div class="w-full max-w-5xl grid grid-cols-1 md:grid-cols-2 rounded-xl shadow-lg overflow-hidden bg-white">
+
+      <div class="hidden md:flex items-center justify-center bg-gradient-to-br from-green-500 to-emerald-600 relative">
+
+        <div class="relative z-10 text-white p-6 max-w-md">
+          <h1 class="text-3xl font-bold">Nutriflow</h1>
+          <p class="mt-2">Organize sua nutrição de forma prática e inteligente.</p>
+        </div>
+      </div>
+
+      <div class="flex flex-col justify-center p-10">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
---
+++ resources/js/pages/Login/Login.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import AuthLayout from "@Layouts/AuthLayout.vue"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+defineOptions({
+  layout: AuthLayout,
+})
+
+</script>
+
+<template>
+<div class="min-h-[50vh] flex flex-col justify-center">
+    <h2 class="text-2xl font-bold mb-6">Acessar conta</h2>
+    <form class="space-y-4">
+      <div >
+        <Label class="mb-2" for="email">E-mail</Label>
+        <Input id="email" type="email" placeholder="seu@email.com" />
+      </div>
+
+      <div>
+        <Label class="mb-2" for="password">Senha</Label>
+        <Input id="password" type="password" placeholder="••••••••" />
+      </div>
+
+      <Button class="w-full mt-4">Entrar</Button>
+    </form>
+
+    <p class="mt-6 text-sm text-gray-500 text-center">
+      Não tem conta?
+      <a href="/sign-up" class="text-green-600 hover:underline">Cadastre-se</a>
+    </p>
+  </div>
+</template>
