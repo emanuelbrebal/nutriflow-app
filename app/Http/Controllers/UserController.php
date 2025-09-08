b02c255eb0a6371f3de6da9e84ee6168c875dbf9
Emanuel <manu.brebal@gmail.com>
2025-09-08 13:01:33 -0300

    feat: Routes and Controllers for Patient and Nutritionist. To cope our first 5 views -> Login, Register, User/MyDashboard, Nutritionist/MyPatients and LandingPage

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function redirectMyDashboard()
+    {
+        // $user = User::where('id', $id)->get();
+        // utilizar middleware de autenticação
+        // passar os dados e relacionamentos do usuário
+        return Inertia::render('User/MyDashboard');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
