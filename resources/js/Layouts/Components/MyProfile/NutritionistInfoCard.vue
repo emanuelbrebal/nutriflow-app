931a79aa8f619eec72144fd82c602807fe4d8b5e
Emanuel <manu.brebal@gmail.com>
2025-10-20 11:05:17 -0300

    feat: Added NutritionistInfoCard to Onboard form and MyProfile view

---
+++ resources/js/Layouts/Components/MyProfile/NutritionistInfoCard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+</script>
+
+<template>
+  <div class="w-full flex flex-row items-center gap-4 rounded shadow bg-white p-6">
+    
+    <div>
+      <Avatar class="w-20 h-20">
+        <AvatarImage src="https://github.com/shadcn.png" alt="Foto do Nutricionista" />
+        <AvatarFallback>ND</AvatarFallback>
+      </Avatar>
+    </div>
+    
+    <div class="flex-1">
+      <p class="text-sm text-gray-500">Seu nutricionista</p>
+      <h3 class="text-xl font-bold text-gray-800">
+        Dr. Nome do Nutricionista
+      </h3>
+      <p class="text-sm text-gray-700">
+        Especialidade (Ex: Nutrição Esportiva)
+      </p>
+      <p classs="text-sm text-gray-500">
+        CRN: 12345/P
+      </p>
+    </div>
+
+    <div>
+      <Button variant="outline">
+        Enviar Mensagem
+      </Button>
+    </div>
+
+  </div>
+</template>
