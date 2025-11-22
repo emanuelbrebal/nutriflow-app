c1972d9b15aa1021f71630bc2b727e6974a6ae40
Emanuel <manu.brebal@gmail.com>
2025-11-21 22:34:05 -0300

    fix: Register request

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -4,6 +4,9 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use App\Enums\AccountTypeEnum; 
+use Illuminate\Validation\Rules\Enum; 
+
 class RegisterRequest extends FormRequest
 {
     /**
@@ -19,49 +22,36 @@ class RegisterRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
-    
 
     // incluir a regra de itens unicos para email quando criar a tabela no banco
     public function rules(): array
     {
         return [
-            'account_type' => 
-            [
-                'required',
-                'string',
-                Rule::in(['nutricionista', 'paciente', 'aluno']),
-
-            ],
-            'email' => 'required|string|email|max:255',
-            'code' => 
-            [
-                'required_if:account_type,paciente',
-                'nullable', 
-                'digits:6',
-                Rule::exists('users', 'referral_code')
-
-            ],
-            'name' => 'required|integer|digits:6',
-            'password' => 'required|string|min:6'
+            'name' => ['required', 'string', 'min:2', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'min:6'], 
+            'account_type' => ['required', new Enum(AccountTypeEnum::class)],
         ];
     }
     public function messages(): array
     {
         return [
             'account_type.required' => "O campo 'Eu sou...' é obrigatório.",
-            'account_type.in' => 'Por favor, selecione um tipo de conta válido.',
+            
+            'account_type.enum' => 'O tipo de conta selecionado é inválido.', 
+
+            'name.required' => "O campo 'Nome Completo' é obrigatório.",
+            'name.string' => "O campo 'Nome Completo' deve ser um nome válido.",
+            'name.min' => "O campo 'Nome Completo' deve ter no mínimo 2 caracteres.",
+            'name.max' => "O campo 'Nome Completo' deve ter no máximo 255 caracteres.",
 
             'email.required' => 'O campo e-mail é obrigatório.',
             'email.email' => 'O endereço de e-mail informado não é válido.',
             'email.string' => 'O endereço de e-mail deve ser um conjunto de caracteres.',
             'email.unique' => 'Este e-mail já está em uso. Por favor, tente outro.',
 
-            'code.required_if' => 'O código do nutricionista é obrigatório para pacientes.',
-            'code.digits' => 'O campo do código é composto por exatamente 6 dígitos.',
-            'code.exists' => 'O código informado é inválido ou não foi encontrado.',
-
             'password.required' => 'Por favor, informe sua senha.',
-            'password.string' => 'A senha deve ser um conjunto de caracteres',
+            'password.confirmed' => 'A confirmação da senha não confere.',
             'password.min' => 'A senha precisa ter no mínimo 6 caracteres',
         ];
     }
