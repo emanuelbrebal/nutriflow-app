eb439cc688fdf6c4d807a91607a0041d0493accd
Emanuel <manu.brebal@gmail.com>
2025-11-24 11:22:40 -0300

    feat: New Enums

--- app/Enums/ObjectivesEnum.php
+++ app/Enums/ObjectivesEnum.php
@@ -8,14 +8,22 @@ enum ObjectivesEnum: int
     case WeightMaintains = 2;
     case MuscleGains = 3;
     case NutritionalReeducate = 4;
-    
+
     public function getLabel(): string
     {
-        return match($this) {
+        return match ($this) {
             self::WeightLoss => 'Perder peso',
             self::WeightMaintains => 'Manter peso',
             self::MuscleGains => 'Ganhar massa muscular',
             self::NutritionalReeducate => 'Reeducação alimentar',
         };
+    }
+
+    public static function options(): array
+    {
+        return array_map(fn($case) => [
+            'value' => $case->value,
+            'label' => $case->getLabel(),
+        ], self::cases());
     }
 }
